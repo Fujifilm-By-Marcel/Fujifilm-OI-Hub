7aa07009da8a134b9cf8944fda11367763ac3ecb
Killeramoeba <killeramoeba@gmail.com>
2020-09-08 15:10:10 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$formAction = "https://test.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8";
+$oidValue = "00D0R0000008xAC";
+
+$affiliatedSectionDepartmentID = "00N1I00000Mifk8";
+$purposeOfVisitID = "00N1I00000KJBMb";
+
+
+?>
---
+++ live-config.php
@@ -0,0 +1,9 @@
+<?php
+$formAction = "https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8";
+$oidValue = "00D1I000001Ye7Y";
+
+$affiliatedSectionDepartmentID = "00N1I00000NKyVz";
+$purposeOfVisitID = "00N1I00000NL7Qt";
+
+
+?>
